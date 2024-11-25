f553c6ecff0a740aadc8f54ed4e6a537a4b04a0c
Prajwal Bharadwaj <prajwalrb17@gmail.com>
2024-11-25 13:34:48 +0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@ const is_loading = ref(true);
 
 onMounted(() => {
   today.value = `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`;
-  window.open(`https://app.dailybot.com/checkins/response-daily/b1d16ce3-7aea-48ee-96d0-406a17e2bae4/add/${today.value}`, '_blank');
+  window.open(`https://app.dailybot.com/checkins/response-daily/b1d16ce3-7aea-48ee-96d0-406a17e2bae4/add/${today.value}`, '_self');
+  // window.open(`https://app.dailybot.com/checkins/response-daily/b1d16ce3-7aea-48ee-96d0-406a17e2bae4/add/${today.value}`, '_blank');
   setTimeout(() => {
     is_loading.value = false;
   }, 2000);
